e6a0359264db692996bc908f1c90da939b4c99da
Ben Hamilton <ben.hamilton@me.com>
2019-04-26 14:53:22 +0100

    created CR from CRUD functionality for the Game class

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,46 @@
 require_relative( "../models/publisher.rb" )
+require_relative( "../models/game.rb" )
 require("pry-byebug")
 
+Game.delete_all()
 Publisher.delete_all()
 
 publisher1 = Publisher.new({
   "name" => "Blizzard"
   })
 
-publisher1.save()
+  publisher1.save()
+
+  publisher2 = Publisher.new(
+    {
+      "name" => "Nintendo"
+    }
+  )
+
+  publisher2.save()
+
+  game1 = Game.new(
+    {
+      "title" => "Overwatch",
+      "stock" => 100,
+      "publisher_id" => publisher1.id
+    }
+  )
+
+  game1.save()
+
+  game2 = Game.new(
+    {
+      "title" => "The Legend of Zelda",
+      "stock" => 1000,
+      "publisher_id" => publisher2.id
+    }
+  )
+
+  game2.save()
+
+
+
 
   binding.pry
   nil
--- models/game.rb
+++ models/game.rb
@@ -2,13 +2,47 @@ require_relative('../db/sql_runner')
 
 class Game
 
-attr_reader :title, :id, :publisher_id
+attr_reader :id, :publisher_id
+attr_accessor :title
 
 def initialize(options)
   @id = options['id'].to_i if options['id']
   @title = options['title']
-  @publisher_id = options['publisher_id'].to_i
   @stock = options['stock'].to_i
+  @publisher_id = options['publisher_id'].to_i
+end
+
+def save()
+  sql = "INSERT INTO games
+  (
+    title,
+    stock,
+    publisher_id
+  )
+  VALUES
+  (
+    $1, $2, $3
+  )
+  RETURNING id"
+  values = [@title, @stock, @publisher_id]
+  results = SqlRunner.run(sql,values)
+  @id = results.first['id'].to_i
+end
+
+def self.all()
+  sql = "SELECT * FROM games"
+  games_data = SqlRunner.run( sql )
+  games = map_items(games_data)
+  return games
+end
+
+def self.delete_all()
+  sql = "DELETE FROM games"
+  SqlRunner.run( sql )
+end
+
+def self.map_items(games_data)
+  return games_data.map { |game| Game.new(game) }
 end
 
 
