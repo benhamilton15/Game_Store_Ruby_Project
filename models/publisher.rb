0490f58df52624714faf0e228abb82d6c4f730ee
Ben Hamilton <ben.hamilton@me.com>
2019-04-26 11:55:13 +0100

    finsihed CRUD functionality for publishers

--- models/publisher.rb
+++ models/publisher.rb
@@ -2,7 +2,8 @@ require_relative('../db/sql_runner')
 
 class Publisher
 
-  attr_reader :name, :id
+  attr_accessor :name
+  attr_reader :id
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
@@ -22,6 +23,14 @@ class Publisher
     values = [@name]
     results = SqlRunner.run(sql,values)
     @id = results.first['id'].to_i
+  end
+
+  def update()
+    sql = "UPDATE publishers
+    SET name = $1
+    WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
   end
 
   def delete()
