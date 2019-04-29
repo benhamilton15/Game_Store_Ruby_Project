a3500aa031b0238e6a2b580000c9d1cb1f98c96a
Ben Hamilton <ben.hamilton@me.com>
2019-04-29 11:02:54 +0100

    created index.erb for games and made basic get request to show all games

---
+++ controllers/games_controller.rb
@@ -0,0 +1,9 @@
+equire( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative("../models/game")
+also_reload( '../models/*' )
+
+get '/games' do
+  @games = Game.all()
+  erb (:"games/index")
+end
