3ed61bd2afb17e859cefb1cf91cc88b256e6259f
Ben Hamilton <ben.hamilton@me.com>
2019-04-30 15:02:17 +0100

    created test file for non database game methods

---
+++ models/specs/game_spec.rb
@@ -0,0 +1,20 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../game')
+
+class TestGame < MiniTest::Test
+
+
+def setup
+
+end
+
+def test_stock_level
+
+end
+
+
+
+
+
+end
